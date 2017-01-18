83327c0369f95fa44947811069939bb5ac9a5942
David Castillo <davecas26@gmail.com>
2017-01-18 11:58:25 -0400

    agregados cambios para los efectos

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
+(function($){
+
 var menu = document.getElementById('scrol-menu');
 var menuItems = document.getElementsByClassName('scroll-menu-item');
+
 var banner = document.getElementById('banner');
 watcherBanner = scrollMonitor.create(banner);
 
-function menuFixed() {
-	watcherBanner.enterViewport(function() {
-		menu.classList.remove('fixed');
-		$(".scroll-content-fixed").removeClass('display');
-	});
+watcherBanner.enterViewport(function() {
+	menu.classList.remove('fixed');
+	//$(".scroll-content-fixed").removeClass('display');
+});
 
-	watcherBanner.exitViewport(function() {
-		menu.classList.add('fixed');
-		$(".scroll-content-fixed").addClass('display');
-	});
-
-}
+watcherBanner.exitViewport(function() {
+	menu.classList.add('fixed');
+	//$(".scroll-content-fixed").addClass('display');
+});
 
 
 /*MENU*/
-
 for (var i = 0; i < menuItems.length; i++){
 	menuItems[i].addEventListener("click", function(event){
 		var selected = {
@@ -27,7 +26,7 @@ for (var i = 0; i < menuItems.length; i++){
 			body: this
 		}
 		goTo(this.attributes['data-name'].nodeValue);
-		menuFixed();
+		activeMenu(selected.id);
 	})
 }
 
@@ -35,40 +34,57 @@ for (var i = 0; i < menuItems.length; i++){
 
 function goTo(el) {
 	$('html, body').animate({
-		scrollTop: $("#"+el).offset().top
+		scrollTop: $("#"+el).offset().top - 82
 	}, 1000);
 }
 
-/*function goTo(el,to,duration) {
-  if (duration < 0) return;
-
-
-  var difference = to - el.scrollTop;
-  
-  if(difference < 0) {
-  	difference = difference * -1;
-  }
-setTimeout(function() {
-    el.scrollTop = el.scrollTop + perTick;
-    scrollTo(el, to, duration - 2);
-  }, 1000);
-  console.log(difference);
+function activeMenu(id) {
+	var currentMenuScroll = $(".active-scroll-menu");
+	var newMenuScroll = $("[data-name='"+id+"']");
+	currentMenuScroll.removeClass('active-scroll-menu');
+	newMenuScroll.addClass("active-scroll-menu");
 }
+/*MENU*/
+
+/*WATCHERS FOR ITEMS*/
+
+var i1 = document.getElementById('item1');
+var i2 = document.getElementById('item2');
+var i3 = document.getElementById('item3');
+var i4 = document.getElementById('item4');
+var i5 = document.getElementById('item5');
+var i6 = document.getElementById('item6');
+watcherItem1 = scrollMonitor.create(i1);
+watcherItem2 = scrollMonitor.create(i2);
+watcherItem3 = scrollMonitor.create(i3);
+watcherItem4 = scrollMonitor.create(i4);
+watcherItem5 = scrollMonitor.create(i5);
+watcherItem6 = scrollMonitor.create(i6);
 
 
-function move(elem) {
+watcherItem1.fullyEnterViewport(function() {
+	activeMenu("item1");
+});
 
-  var left = 0
+watcherItem2.fullyEnterViewport(function() {
+	activeMenu("item2");
+});
 
-  function frame() {
-    
-    left++  // update parameters 
-    
-    elem.style.left = left + 'px' // show frame 
+watcherItem3.fullyEnterViewport(function() {
+	activeMenu("item3");
+});
 
-    if (left == 100)  // check finish condition
-      clearInterval(id)
-  }
+watcherItem4.fullyEnterViewport(function() {
+	activeMenu("item4");
+});
 
-  var id = setInterval(frame, 10) // draw every 10ms
-}*/
+watcherItem5.fullyEnterViewport(function() {
+	activeMenu("item5");
+});
+
+watcherItem6.fullyEnterViewport(function() {
+	activeMenu("item6");
+});
+
+/*WATCHERS FOR ITEMS*/
+})(jQuery);
